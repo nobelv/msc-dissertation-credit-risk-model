098d78154af14029c48164997eeb3f595f005c64
Victor Nobel <nobel.victor@gmail.com>
2018-02-13 20:47:23 +0100

    Added data_functions.py. These functions are used to annualize the quarterly EBITDA, Interest Expense as well as the Capex which comes in a quarterly YTD form.

---
+++ Python Code/data_functions.py
@@ -0,0 +1,79 @@
+def annualize_quarterly(keys, data_dict, fqtr_dict):
+    """
+    Annualizes the quarterly EBITDA or Interest Expense data using the following rules:
+    Q1_ann = Q1 * 4
+    Q2_ann = (Q2 + Q1) * 2
+    Q3_ann = (Q3 + Q2 + Q1) + ((Q3 + Q2 + Q1) / 3)
+    Q4_ann = Q4 + Q3 + Q2 + Q1
+
+    :param keys: The list containing all unique gvkeys for which we want to annualize data.
+    :param data_dict: The dictionary containing our to be annualized data.
+    :param fqtr_dict: The dictionary containing the quarters.
+    :return: Annualized data per quarter.
+    """
+
+    data_ann_dict = {}
+
+    for i in range(len(keys)):
+        k = keys[i]
+        fqtr = fqtr_dict[k].tolist()
+        data = data_dict[k].tolist()
+        annual_list = []
+
+        for ii in range(len(data)):
+            if fqtr[ii] == 1:
+                annual = data[ii] * 4
+                annual_list.append(annual)
+            elif fqtr[ii] == 2:
+                annual = (data[ii] + data[ii - 1]) * 2
+                annual_list.append(annual)
+            elif fqtr[ii] == 3:
+                summed = data[ii] + data[ii - 1] + data[ii - 2]
+                annual = summed + (summed / 3)
+                annual_list.append(annual)
+            else:
+                annual = data[ii] + data[ii - 1] + data[ii - 2] + data[ii - 3]
+                annual_list.append(annual)
+            data_ann_dict.update({k: annual_list})
+
+    return data_ann_dict
+
+
+def annualize_qytd(keys, data_dict, fqtr_dict):
+    """
+    Annualize quarterly YTD data, for example capxytd, using the following rules:
+    Q1_ann = Q1 * 4
+    Q2_ann = Q2 * 2
+    Q3_ann = Q3 + (Q3 / 3)
+    Q4_ann = Q4
+
+    :param keys: The list containing all unique gvkeys for which we want to annualize data.
+    :param data_dict: The dictionary containing our to be annualized data.
+    :param fqtr_dict: The dictionary containing the quarters.
+    :return: Annualized data per quarter.
+    """
+
+    data_ann_dict = {}
+
+    for i in range(len(keys)):
+        k = keys[i]
+        fqtr = fqtr_dict[k].tolist()
+        data = data_dict[k].tolist()
+        annual_list = []
+
+        for ii in range(len(data)):
+            if fqtr[ii] == 1:
+                annual = data[ii] * 4
+                annual_list.append(annual)
+            elif fqtr[ii] == 2:
+                annual = data[ii] * 2
+                annual_list.append(annual)
+            elif fqtr[ii] == 3:
+                annual = data[ii] + (data[ii] / 3)
+                annual_list.append(annual)
+            else:
+                annual_list.append(data[ii])
+
+        data_ann_dict.update({k: annual_list})
+
+    return data_ann_dict
