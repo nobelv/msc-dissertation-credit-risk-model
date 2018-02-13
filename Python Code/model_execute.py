b0777400f5d94b68c15e022a5a82e7333d38cd83
Victor Nobel <nobel.victor@gmail.com>
2018-02-13 20:51:46 +0100

    Fixed the pandas imports. Completed most of the execution script, fixed most of the model. Fixed indentation of quadratic function.

--- Python Code/model_execute.py
+++ Python Code/model_execute.py
@@ -4,9 +4,10 @@
 
 # Model Execution file, requires model_functions.py & securities_functions.py!
 import model_functions as mf
+import data_functions as dfunc
+import scipy.optimize as sp
 import newtonraphson as nr
 import pandas as pd
-from operator import itemgetter
 import timeit
 
 ########################################################################################################################
@@ -19,9 +20,14 @@ start_time = timeit.default_timer()
 ########################################################################################################################
 
 # Reading the data into a dataframe from our csv files.
-df = pd.read_csv("g:/na_data.csv", sep=';')
-df2 = pd.read_csv("g:/na_treasury.csv", sep=';')
-df2['thedate'] = pd.to_datetime(df2['thedate'])
+df = pd.read_csv("g:/na_quarterly_1990.csv", sep=';', dtype={"gvkey": int, "cusip": str, "conml": str, "datadate": str,
+                                                             "fyearq": int, "fqtr": int, "niq": float, "ltq": float,
+                                                             "seqq": float, "capxytd": float, "tieq": float,
+                                                             "xintq": float, "intexp": float, "txtq": float,
+                                                             "dpq": float, "ebitda": float, "prccq": float,
+                                                             "cshoq": float, "equityobs": float})
+df2 = pd.read_csv("g:/na_treasury2.csv", sep=';')
+df2['thedate'] = pd.to_datetime(df2['thedate'], format="%Y%m%d")
 df2 = df2.set_index(['thedate'])
 
 keys = df.gvkey.unique()
@@ -29,29 +35,48 @@ ebitda_dict = {}
 liabilities_dict = {}
 coupon_dict = {}
 capex_dict = {}
+intexp_dict = {}
 fyear_dict = {}
+fqtr_dict = {}
+date_dict = {}
+equityobs_dict = {}
 
 # Populating our dictionaries with data specific to each company.
-for i in range(len(keys)):
-    k = keys[i]
-    data = df.loc[df['gvkey'] == k]
-    ebitda = data['ebitda']
-    liabilities = data['liabilities']
-    couponrate = data['couponrate']
-    capex = data['capex']
-    fyear = data['fyear']
-    ebitda_dict.update({k: ebitda})
+for key in range(len(keys)):
+    k = keys[key]
+    dataframe = df.loc[df['gvkey'] == k]
+    qtr = dataframe['fqtr']
+    t = dataframe['ebitda']
+    intexp = dataframe['intexp']
+
+    ebitda_dict.update({k: t})
+    intexp_dict.update({k: intexp})
+    fqtr_dict.update({k: qtr})
+
+    liabilities = dataframe['ltq']
+    capex = dataframe['capxytd']
+    fyear = dataframe['fyearq']
+
     liabilities_dict.update({k: liabilities})
-    coupon_dict.update({k: couponrate})
     capex_dict.update({k: capex})
     fyear_dict.update({k: fyear})
 
+    date = dataframe['datadate']
+    date_dict.update({k: date})
+
+    equityobs = dataframe['equityobs']
+    equityobs_dict.update({k: equityobs})
+
+# Annualize ebitda, interest expense and capex
+
+ebitda_ann = dfunc.annualize_quarterly(keys, ebitda_dict, fqtr_dict)
+intexp_ann = dfunc.annualize_quarterly(keys, intexp_dict, fqtr_dict)
+capex_ann = dfunc.annualize_qytd(keys, capex_dict, fqtr_dict)
 
 ########################################################################################################################
 # Executing the model
 ########################################################################################################################
 
-# Obtain sigma and miu_delta
 miu_delta_dict = {}
 sigma_dict = {}
 for i in range(len(keys)):
@@ -59,34 +84,67 @@ for i in range(len(keys)):
     # Populate dictionaries with sigma & miu_delta values
     sigma = mf.func_sigma(k, ebitda_dict)
     sigma_dict.update({k: sigma[k]})
-    miudelta = mf.miu_delta(k, ebitda_dict, fyear_dict, sigma[k])
+    miudelta = mf.miu_delta(k, ebitda_ann, fyear_dict, sigma[k])
     miu_delta_dict.update({k: miudelta})
 
-# Obtain risk free rate and calculate mbar
-for i in range(len(keys)):
-    k = keys[i]
-    # get the first value in ebitda and fyear per company
-    delta_0 = itemgetter(0)(ebitda_dict[k].tolist())
-    year0 = str(itemgetter(0)(fyear_dict[k].tolist()))
+    for t in range(len(ebitda_ann[k])):
+        ebitdalist = ebitda_ann[k]
+        capex_list = capex_ann[k]
+        intexp_list = intexp_ann[k]
+        delta_0 = ebitdalist[t]
 
-    start_date = "{}-12-01".format(year0)
-    end_date = "{}-12-31".format(year0)
+        dates = date_dict[k].tolist()
+        rf_rate = df2.iloc[df2.index.get_loc(dates[t], method="nearest")].tolist()
 
-    temp = df2.loc[start_date:end_date]
-    rf_rate = temp['tenyr_rate'].tail(1)
-    rf_rate = rf_rate.tolist()
+        liabilities_list = liabilities_dict[k].tolist()
 
-    def quadratic(x):
-        return float(rf_rate[0]) + float(x) * sigma_dict[k]
+        coupondata_dict = {'liabilities': liabilities_list, 'interest_exp': intexp_list}
+        coupon_df = pd.DataFrame(coupondata_dict)
+        coupon_df['coupon_rate'] = coupon_df['interest_exp'] / coupon_df['liabilities']
+        couponrate = coupon_df['coupon_rate'].tolist()
 
-    mbar = nr.solve(quadratic, 1, 0.01)
-    miua = mf.miu_big_a(rf_rate[0], mbar, sigma_dict[k])
-    biga0 = mf.big_a_0(delta_0, miua, miu_delta_dict[k])
-    print(rf_rate[0])
-    print(sigma_dict[k])
-    print(mbar)
-    print(miua)
-    print(biga0)
+        equityobserved = equityobs_dict[k].tolist()
+
+        def quadratic(x):
+            return (mf.effective_taxrate("usa") *
+                    (mf.payout_0(delta_0, rf_rate[0],
+                                 mf.v_star(miu_delta_dict[k], x, sigma_dict[k]), sigma_dict[k],
+                                 mf.big_r(mf.v_bar(sigma_dict[k], mf.v_star(miu_delta_dict[k], x, sigma_dict[k]),
+                                                   rf_rate[0], x, miu_delta_dict[k], couponrate[t], liabilities_list[t],
+                                                   mf.small_omega(rf_rate[0], sigma_dict[k],
+                                                                  mf.v_star(miu_delta_dict[k], x, sigma_dict[k])),
+                                                   mf.small_a(mf.v_star(miu_delta_dict[k], x, sigma_dict[k]),
+                                                              sigma_dict[k]), capex_list[t]),
+                                          mf.big_a_0(delta_0, mf.miu_big_a(rf_rate[0], x, sigma_dict[k]),
+                                                     miu_delta_dict[k])), mf.small_a(
+                            mf.v_star(miu_delta_dict[k], x,sigma_dict[k]), sigma_dict[k])) -
+
+                     mf.coupon_0(couponrate[t], liabilities_list[t], mf.var_pi(rf_rate[0]),
+                                 mf.v_star(miu_delta_dict[k], x, sigma_dict[k]), sigma_dict[k],
+                                 mf.big_r(mf.v_bar(sigma_dict[k], mf.v_star(miu_delta_dict[k], x, sigma_dict[k]),
+                                                   rf_rate[0], x, miu_delta_dict[k], couponrate[t], liabilities_list[t],
+                                                   mf.small_omega(rf_rate[0], sigma_dict[k],
+                                                                  mf.v_star(miu_delta_dict[k], x, sigma_dict[k])),
+                                                   mf.small_a(mf.v_star(miu_delta_dict[k], x, sigma_dict[k]),
+                                                              sigma_dict[k]), capex_list[t]),
+                                          mf.big_a_0(delta_0, mf.miu_big_a(rf_rate[0], x, sigma_dict[k]),
+                                                     miu_delta_dict[k])), mf.small_a(
+                             mf.v_star(miu_delta_dict[k], x, sigma_dict[k]), sigma_dict[k])) -
+
+                     mf.capex_0(capex_list[t], mf.var_pi(rf_rate[0]), mf.v_star(miu_delta_dict[k], x, sigma_dict[k]),
+                                sigma_dict[k],
+                                mf.big_r(mf.v_bar(sigma_dict[k], mf.v_star(miu_delta_dict[k], x, sigma_dict[k]),
+                                                  rf_rate[0], x, miu_delta_dict[k], couponrate[t], liabilities_list[t],
+                                                  mf.small_omega(rf_rate[0], sigma_dict[k],
+                                                                 mf.v_star(miu_delta_dict[k], x, sigma_dict[k])),
+                                                  mf.small_a(mf.v_star(miu_delta_dict[k], x, sigma_dict[k]),
+                                                             sigma_dict[k]), capex_list[t]),
+                                         mf.big_a_0(delta_0, mf.miu_big_a(rf_rate[0], x, sigma_dict[k]),
+                                                    miu_delta_dict[k])), mf.small_a(
+                             mf.v_star(miu_delta_dict[k], x, sigma_dict[k]), sigma_dict[k])))) - equityobserved[t]
+
+        mbar = sp.newton(quadratic, 0.31, fprime2=nr.derivative(quadratic, 0.4, 0.01), maxiter=100000)
+        print("mbar: ", mbar, "for company:", k, "at date", dates[t])
 
 ########################################################################################################################
 # Stopping the timer
